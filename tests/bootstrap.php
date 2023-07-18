bfdf10d3280104903f9303a94cb0c577a5a5f4bd
igramos11 <98900181+igramos11@users.noreply.github.com>
2023-07-17 21:23:36 -0500

    tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+    require_once "php/config.php";
+
+    // $User_ID = "6";
+    // $firstName = "David";
+    // $lastName = "Silva";
+    // $email = "david.silva.25812@gmail.com";
+    // $phoneNumber = "8326913979";
+    // $Address = "4361 Cougar Village Drive";
+    // $APT = NULL;
+    // $City = "Houston";
+    // $State = "TX";
+    // $ZipCode = "77204";
+    // $Username = "jetscooters";
+?>
