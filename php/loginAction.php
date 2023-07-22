4cc25582556346d10cd6193dd3fd3eba8141acdc
J-Dawg132 <111777662+J-Dawg132@users.noreply.github.com>
2023-07-21 22:39:43 -0500

    Login

--- php/loginAction.php
+++ php/loginAction.php
@@ -6,32 +6,41 @@ session_start();
 
 if (isset($_POST['uname']) && isset($_POST['psw'])) {
     $username = validate($_POST['uname']);
-    $password = $_POST['psw']; // We don't hash this password here
+    $password = $_POST['psw'];
 
     $user = getUserByUsername($conn, $username); // Get the user by username
 
     if ($user === false) {
-        header("Location: ../pages/login.php?invalid=username");
+        header("Location: ../pages/login.php?invalid=true");
         exit();
     }
 
     // Verify the password
-    $passwordCheck = password_verify($password, $user['Password']); 
+    $passwordCheck = password_verify($password, $user['Password']);
 
     if ($passwordCheck === false) {
-        header("Location: ../pages/login.php?invalid=password");
+        header("Location: ../pages/login.php?invalid=true");
         exit();
     }
 
-    // If password is valid, start a new session and log the user in
+    // If username and password are valid, start a new session and log the user in
     $_SESSION['login_user'] = $username;
     $_SESSION['user_id'] = $user['User_ID'];
 
-    header("Location: ../pages/formCompleteProfile.php");
-    exit();
+    // Check if it's the user's first login and if so, direct them to completeProfile.php
+    if ($user['first_login']) {
+        // Update the 'first_login' flag in the database (assumes setFirstLoginFlag() is a function you've created)
+        setFirstLoginFlag($conn, $username, false);
+        header("Location: ../pages/completeProfile.php");
+        exit();
+    } else {
+        // For successive logins, redirect to user dashboard
+        header("Location: ../pages/userDashboard.php");
+        exit();
+    }
 } else {
     header("Location: ../pages/login.php?invalid=true");
     exit();
 }
-// @codeCoverageIgnoreEnd
 ?>
+
